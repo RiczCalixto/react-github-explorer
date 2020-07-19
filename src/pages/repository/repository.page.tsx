592453f03cd49cdd62fa40d2217f6118c5e1ae04
RiczCalixto <rcalixtos@gmail.com>
2020-07-18 21:20:58 -0300

    style repository page

--- src/pages/repository/repository.page.tsx
+++ src/pages/repository/repository.page.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import {
+  Header,
+  RepositoryInfo,
+  IssuesList,
+} from '../../styled-components/components.styles';
+import { ListItem } from '../dashboard/list-item.component';
 
 interface NavigationParams {
   fullName: string;
@@ -8,5 +16,47 @@ interface NavigationParams {
 export const RepositoryPage: React.FC = () => {
   const { params } = useRouteMatch<NavigationParams>();
 
-  return <h1>RIcorico{params.fullName}</h1>;
+  return (
+    <>
+      <Header>
+        <Link to="/">
+          <FiChevronLeft size={20} />
+          Voltar
+        </Link>
+        <img src={logoImg} alt="Github Explorer icon" />
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img src="https://avatars2.githubusercontent.com/u/739839?s=200&v=4" />
+          <div>
+            <strong>fulleName</strong>
+            <span>description</span>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1080</strong>
+            <span>star</span>
+          </li>
+          <li>
+            <strong>123</strong>
+            <span>forks</span>
+          </li>
+          <li>
+            <strong>77</strong>
+            <span>issues</span>
+          </li>
+        </ul>
+        <IssuesList>
+          <Link to={`/repository/`}>
+            <div>
+              <strong>Casa</strong>
+              <p>Joana</p>
+            </div>
+            <FiChevronRight size={20} />
+          </Link>
+        </IssuesList>
+      </RepositoryInfo>
+    </>
+  );
 };
--- src/styled-components/components.styles.ts
+++ src/styled-components/components.styles.ts
@@ -32,7 +32,7 @@ export const SearchForm = styled.form<SearchFormProps>`
 export const RepositoriesList = styled.div`
   a {
     ${tw`bg-white rounded-md w-full p-6 block flex items-center
-    transform hover:translate-x-4 duration-500`}
+    transform hover:translate-x-4 duration-500 border border-blue-500`}
 
     img {
       ${tw`w-16 h-16 rounded-full`}
@@ -63,4 +63,83 @@ export const RepositoriesList = styled.div`
 export const Error = styled.div`
   ${tw`text-red-600 bg-red-100 border border-red-400
   text-red-700 px-4 py-3 rounded max-w-screen-sm my-6`}
+`;
+
+export const Header = styled.header`
+  ${tw`flex items-center justify-between`}
+  a {
+    ${tw`flex items-center text-gray-600 transform hover:text-gray-800
+    duration-300`}
+    svg {
+      ${tw`mr-2`}
+    }
+  }
+`;
+
+export const RepositoryInfo = styled.section`
+  ${tw`mt-20`}
+
+  header {
+    ${tw`flex items-center`}
+
+    img {
+      ${tw`w-24 h-24 border-4 border-blue-500 rounded-full`}
+    }
+
+    div {
+      ${tw`flex flex-col ml-2`}
+
+      strong {
+        ${tw`text-2xl text-gray-600`}
+      }
+
+      span {
+        ${tw`text-lg text-gray-500`}
+      }
+    }
+  }
+  ul {
+    ${tw`flex mt-4`}
+
+    li {
+      strong {
+        ${tw`block text-lg text-gray-600`}
+      }
+      span {
+        ${tw`block mt-2 text-base text-gray-500`}
+      }
+
+      & + li {
+        ${tw`ml-20`}
+      }
+    }
+  }
+`;
+
+export const IssuesList = styled.div`
+  ${tw`mt-20`}
+  a {
+    ${tw`bg-white rounded-md w-full p-6 block flex items-center
+    transform hover:translate-x-4 duration-500 border border-blue-500`}
+
+    & + a {
+      ${tw`mt-4`}
+    }
+
+    div {
+      ${tw`mx-5 flex flex-col`}
+
+      strong {
+        ${tw`text-gray-600 text-lg`}
+      }
+
+      p {
+        ${tw`text-gray-500 text-base `}
+      }
+    }
+
+    svg {
+      ${tw`ml-auto text-gray-600`}
+    }
+  }
 `;

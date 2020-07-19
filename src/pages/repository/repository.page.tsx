79fe59db85ef6ea7dfd5da0c45f025c338ffc383
RiczCalixto <rcalixtos@gmail.com>
2020-07-18 22:21:04 -0300

    List repository issues

--- src/pages/repository/repository.page.tsx
+++ src/pages/repository/repository.page.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
@@ -7,14 +7,56 @@ import {
   RepositoryInfo,
   IssuesList,
 } from '../../styled-components/components.styles';
-import { ListItem } from '../dashboard/list-item.component';
+import { api } from '../../services/api';
 
 interface NavigationParams {
   fullName: string;
 }
 
+interface RepositoryDetailsState {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+interface IssueState {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  };
+}
+
 export const RepositoryPage: React.FC = () => {
   const { params } = useRouteMatch<NavigationParams>();
+  const [repository, setRepository] = useState<RepositoryDetailsState | null>(
+    null,
+  );
+  const [issues, setIssues] = useState<IssueState[]>([]);
+
+  useEffect(() => {
+    async function loadData(): Promise<void> {
+      const [remoteRepository, remoteIssues] = await Promise.all([
+        api.get(`/repos/${params.fullName}`),
+        api.get(`/repos/${params.fullName}/issues`),
+      ]);
+      console.log(remoteRepository.data);
+      console.log(remoteIssues.data);
+      setRepository(remoteRepository.data);
+      setIssues(remoteIssues.data);
+      // setRepository(remoteRepository.data);
+      // setIssues(remoteIssues.data);
+    }
+
+    loadData();
+  }, [params.fullName]);
 
   return (
     <>
@@ -27,36 +69,38 @@ export const RepositoryPage: React.FC = () => {
       </Header>
       <RepositoryInfo>
         <header>
-          <img src="https://avatars2.githubusercontent.com/u/739839?s=200&v=4" />
+          <img src={repository?.owner?.avatar_url} />
           <div>
-            <strong>fulleName</strong>
-            <span>description</span>
+            <strong>{repository?.full_name}</strong>
+            <span>{repository?.description}</span>
           </div>
         </header>
         <ul>
           <li>
-            <strong>1080</strong>
+            <strong>{repository?.stargazers_count}</strong>
             <span>star</span>
           </li>
           <li>
-            <strong>123</strong>
+            <strong>{repository?.forks_count}</strong>
             <span>forks</span>
           </li>
           <li>
-            <strong>77</strong>
+            <strong>{repository?.open_issues_count}</strong>
             <span>issues</span>
           </li>
         </ul>
-        <IssuesList>
-          <Link to={`/repository/`}>
+      </RepositoryInfo>
+      <IssuesList>
+        {issues?.map(issue => (
+          <a key={issue.id} href={issue.html_url}>
             <div>
-              <strong>Casa</strong>
-              <p>Joana</p>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
             </div>
             <FiChevronRight size={20} />
-          </Link>
-        </IssuesList>
-      </RepositoryInfo>
+          </a>
+        ))}
+      </IssuesList>
     </>
   );
 };
